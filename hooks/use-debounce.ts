c7d25e5cd9d728e4163cf3e9f8226b7e77e8cf37
Manou <manouhaouzi@gmail.com>
2023-08-06 21:08:19 +0300

    add search input

---
+++ hooks/use-debounce.ts
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react'
+
+export const UseDebounce = <T>(value: T, delay?: number): T => {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(value)
+    }, delay || 500)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [value, delay])
+  return debouncedValue
+}
