2a9342e7007953c8c8d303d62b76a604fddd05e8
Manou <manouhaouzi@gmail.com>
2023-08-08 15:02:56 +0300

    category in companion facultative

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -26,9 +26,14 @@ async function main() {
     //     },
     //   ],
     // })
+
     // const { companion } = db
     // const deleteResult = await companion.deleteMany()
     // console.log(`Deleted ${deleteResult.count} companions.`)
+
+    const { companion } = db
+    const result = await companion.findMany()
+    console.log('result: ', result)
   } catch (error) {
     console.error('Error seeding default categories', error)
   } finally {
