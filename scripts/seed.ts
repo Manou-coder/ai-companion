5ddc6ce4a6f67906dd4bc8345385c6cbb76d075b
Manou <manouhaouzi@gmail.com>
2023-08-09 22:46:13 +0300

    category facultatif

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -4,36 +4,38 @@ const db = new PrismaClient()
 
 async function main() {
   try {
-    // await db.category.createMany({
-    //   data: [
-    //     {
-    //       name: 'Famous people',
-    //     },
-    //     {
-    //       name: 'Movies & TV',
-    //     },
-    //     {
-    //       name: 'Musicians',
-    //     },
-    //     {
-    //       name: 'Animals',
-    //     },
-    //     {
-    //       name: 'Philosophy',
-    //     },
-    //     {
-    //       name: 'Scientists',
-    //     },
-    //   ],
-    // })
+    await db.category.createMany({
+      data: [
+        {
+          name: 'Famous people',
+        },
+        {
+          name: 'Movies & TV',
+        },
+        {
+          name: 'Musicians',
+        },
+        {
+          name: 'Animals',
+        },
+        {
+          name: 'Philosophy',
+        },
+        {
+          name: 'Scientists',
+        },
+      ],
+    })
 
     // const { companion } = db
     // const deleteResult = await companion.deleteMany()
     // console.log(`Deleted ${deleteResult.count} companions.`)
 
-    const { companion } = db
-    const result = await companion.findMany()
-    console.log('result: ', result)
+    // const { companion, category } = db
+    // const result = await companion.deleteMany()
+    // const result1 = await category.deleteMany()
+    // console.log('result1: ', result1)
+    // console.log('result: ', result)
   } catch (error) {
     console.error('Error seeding default categories', error)
   } finally {
